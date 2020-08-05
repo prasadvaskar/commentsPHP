a23b48660bd7ff3ba0de49512c21ee974cacb17a
Prasa Vaskar <vaskarprasad123@gmail.com>
2020-08-05 21:13:41 +0530

    Data Entry CSS done

--- index.php
+++ index.php
@@ -2,18 +2,27 @@
 
 <head>
     <title>Enter Details</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
+    
 </head>
 
 <body>
-    <div>
-        <form action="postingdata.php" method="POST">
+    <div class ="div1" id="box">
+        <form id="for1" action="postingdata.php" method="POST">
         
             <input type="text" id="name" name="name" placeholder="Enter Name" required>
             <input type="email" id="email" name="email" placeholder="Enter Email" required>
             <input type="text" id="comment" name="comment" placeholder="Comment Here..." required>
-            <button type ="submit">Submit</button>
+            
+            <button id ="btn" type ="submit">Submit</button>
         </form>
+        
     </div>
 </body>
+
 
 </html>
--- postingdata.php
+++ postingdata.php
@@ -1,3 +1,9 @@
+<html>
+    <body>
+        <h3>New record created successfully ! You will be redirected automatically in few seconds</h3>
+        <img src="css/images/3.gif" alt="">
+    </body>
+</html>
 <?php
 session_start();
 require_once('connection.php');
@@ -10,13 +16,14 @@ $comment = $_POST['comment'];
 $sql = "INSERT INTO userdetails (name,email,comment)
 VALUES('$name','$email','$comment')";
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully ! You will be redirected automatically in few seconds";
-   // header('Refresh: 5; URL=opportunities.php');
+    
+    header('Refresh: 3; URL=commentpage.php');
  } else {
     echo "Error: " . $sql . "
 " . mysqli_error($conn);
  }
-
+ 
+ 
 
 $conn->close();
 
